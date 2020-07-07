959f8877977585ffc47fb034705c02dfed9ae46c
marcos.paulo.d.souza <marcos.paulo.d.souza@accenture>
2020-07-07 18:05:06 -0300

    first commit. Creating project

---
+++ src/main/java/com/study/springbatch/springbatch/api/SpringbatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.springbatch.springbatch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/springbatch/springbatch/api/SpringbatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springbatch.springbatch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
