3a0df241d75533615f402aee160f6969d60e5aaa
marcos.paulo.d.souza <marcos.paulo.d.souza@accenture>
2020-07-07 21:56:44 -0300

    adding class of configuration

--- src/main/java/com/study/springbatch/springbatch/api/SpringbatchApiApplication.java
+++ src/main/java/com/study/springbatch/springbatch/api/SpringbatchApiApplication.java
@@ -3,6 +3,12 @@ package com.study.springbatch.springbatch.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Marcos Paulo
+ * @version 1.0
+ * @since 1.0
+ * 
+ */
 @SpringBootApplication
 public class SpringbatchApiApplication {
 
---
+++ src/main/java/com/study/springbatch/springbatch/api/configuration/BatchConfiguration.java
@@ -0,0 +1,26 @@
+package com.study.springbatch.springbatch.api.configuration;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Marcos Paulo
+ * @since 1.0
+ * 
+ */
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+
+}
